e3c3b2878f940ae3a3e496dd72e1e09e2ca11837
nezizzi <zizzi.n@husky.neu.edu>
2017-11-19 13:28:00 -0500

    label maps

--- PS01/index.js
+++ PS01/index.js
@@ -1,33 +1,25 @@
-var width = document.getElementById('svg1').clientWidth;
-var height = document.getElementById('svg1').clientHeight;
+var margin = {top: 66, right: 110, bottom: 20, left: 188},
+    width = document.body.clientWidth - margin.left - margin.right,
+    height = 340 - margin.top - margin.bottom,
+    innerHeight = height - 2;
 
-var marginLeft = 50;
-var marginTop = 50;
+
+var scaleX= d3.scaleBand().range([0, width]);
+var scaleY= [];
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
 
 var nestedData = [];
 var formerDancers;
 var currentDancers;
-var testMap = d3.map();
+var Map = d3.map();
 clicked=false;
-
-//circle variables
-var circles;
-var circle_axis;
-var circles2;
-var circle_axis2;
-var center_x=150;
-var center_y=175;
-var R=20;
-var center_x2=150;
-var center_y2=175;
-
-var svg = d3.select('#svg1')
-    .append('g')
-    .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
-
-var svg2 = d3.select('#svg2')
-    .append('g')
-    .attr('transform', 'translate(' + marginLeft + ',' + marginTop + ')');
 
 
 var axislabel = [{value: 1, text: "None"},
@@ -43,7 +35,7 @@ var axislabel = [{value: 1, text: "None"},
 
 
 var LABEL= axislabel.forEach(function (d) {
-    testMap.set(d.value, d.text);
+    Map.set(d.value, d.text);
 });
 
 
@@ -58,15 +50,67 @@ var axislabel2 = [{value: 1, text: "Completed Primary School"},
 
 
 var LABEL2= axislabel2.forEach(function (d) {
-    testMap.set(d.value, d.text);
+    Map.set(d.value, d.text);
 });
+
+
+var whyStopCurrentLabel= [ {value: 1, text: "Feeling to old to Continue"},
+    {value: 2, text: "Financial Difficulties"},
+    {value: 3, text: "Health/effect of Injuries"},
+    {value: 4, text: "Dance work not likely to be available"},
+    {value: 5, text: "Desire to move to a new Career"},
+    {value: 6, text: "Contract Expires"},
+    {value: 7, text: "Other"},
+    {value: 8, text: "Don't know"},
+    {value: 9, text: "Family Responsibilities/Desire to Start a Family"},
+    {value: 10, text: "When it is no longer enjoyable"},
+    {value: "D", text: "Did not answer"}
+];
+
+var challengesCurrentLabel= [ {value: 1, text: "Physical Problems"},
+    {value: 2, text: "Loss of Status"},
+    {value: 3, text: "Loss of Income"},
+    {value: 4, text: "Loss of Friends and Support Network"},
+    {value: 5, text: "Emotional Problems"},
+    {value: 6, text: "Difficulty Deciding What to do Next"},
+    {value: 7, text: "A Sense of Emptiness"},
+    {value: 8, text: "Other"},
+    {value: 9, text: "Don't know"},
+    {value: "D", text: "Did not answer"}
+];
+
+
+
+var whyStopFormerLabel= [ {value: 1, text: "Feeling to old to Continue"},
+    {value: 2, text: "Financial Difficulties"},
+    {value: 3, text: "Health/effect of Injuries"},
+    {value: 4, text: "Dance work not available"},
+    {value: 5, text: "Desire to move to a new Career"},
+    {value: 6, text: "Contract Expired"},
+    {value: 7, text: "Other"},
+    {value: 8, text: "Don't know"},
+    {value: 9, text: "Family Responsibilities"},
+    {value: 10, text: "No longer enjoyable"},
+    {value: "D", text: "Did not answer"}
+];
+
+
+var challengesFormerLabel= [ {value: 1, text: "Physical Problems"},
+    {value: 2, text: "Loss of Status"},
+    {value: 3, text: "Loss of Income"},
+    {value: 4, text: "Loss of Friends and Support Network"},
+    {value: 5, text: "Emotional Problems"},
+    {value: 6, text: "Difficulty Deciding What to do Next"},
+    {value: 7, text: "A Sense of Emptiness"},
+    {value: 8, text: "Other"},
+    {value: 9, text: "Don't Remember"},
+    {value: "D", text: "Did not answer"}
+];
 
 var div = d3.select("body").append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-//console.log(testMap);
-//console.log(testMap.get(5));
 
 
 //import the data from the .csv file
@@ -80,93 +124,9 @@ d3.csv('./data.csv', function(dataIn){
     currentDancers = nestedData.filter(function(d){return d.key == '1'})[0].values;
     formerDancers = nestedData.filter(function(d){return d.key == '2'})[0].values;
 
-   svg.append('text')
-       .text('Highest Level Dance Education')
-       .attr('transform','translate(20, 0)');
-   svg2.append('text')
-       .text('Highest Level Non-Dance Education')
-       .attr('transform','translate(10, 0)');
 
 
-    svg.selectAll('line')
-        .data(currentDancers, function(d){return d.A6QUALS1;})
-        .enter()
-        .append('line');
 
-    //circle axes
-    circles=svg.selectAll('circle')
-        .data(axislabel)
-        .enter()
-        .append('circle');
-
-    circle_axis= circles
-        .attr("cx", center_x)
-        .attr("cy", center_y)
-        .attr("r", function(d){
-            //console.log(d.value);
-            return R*d.value
-        })
-        .attr("stroke", "black")
-        .attr('stoke-width', '10')
-        .attr("fill", "none")
-        .attr('data-toggle', 'tooltip')
-        .attr('title', function(d){
-            return  d.text;
-        })
-        .on("mouseover", function(d) {
-            div.transition()
-                .duration(10)
-                .style("opacity", .9);
-            div.html(d.text)
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function(d) {
-            div.transition()
-                .duration(1000)
-                .style("opacity", 0);
-        });
-
-    svg2.selectAll('line')
-        .data(currentDancers, function(d){return d.A7GENED;})
-        .enter()
-        .append('line');
-
-    circles2=svg2.selectAll('circle')
-        .data(axislabel2)
-        .enter()
-        .append('circle');
-
-    circle_axis2= circles2
-        .attr("cx", center_x2)
-        .attr("cy", center_y2)
-        .attr("r", function(d){
-            //console.log(d.value);
-            return R*d.value
-        })
-        .attr("stroke", "black")
-        .attr('stoke-width', '10')
-        .attr("fill", "none")
-        .attr('data-toggle', 'tooltip')
-        .attr('title', function(d){
-            return  d.text;
-        })
-        .on("mouseover", function(d) {
-            div.transition()
-                .duration(10)
-                .style("opacity", .9);
-            div.html(d.text)
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY - 28) + "px");
-        })
-        .on("mouseout", function(d) {
-            div.transition()
-                .duration(1000)
-                .style("opacity", 0);
-        });
-
-
-    //call the drawPoints function below, and hand it the data2016 variable with the 2016 object array in it
     drawPoints(currentDancers);
 
 });
@@ -175,99 +135,12 @@ d3.csv('./data.csv', function(dataIn){
 
 function drawPoints(pointData){
 
-   var lines = svg.selectAll('line')
-       .data(pointData, function(d){
-           //console.log(d.A6QUALS1);
-           return d.A6QUALS1;
-       });
 
-    //look to see if there are any old bars that don't have keys in the new data list, and remove them.
-    lines.exit()
-        .remove();
-
-    //update the properties of the remaining bars (as before)
-    lines
-        .append('line')
-        .attr('x1',center_x)
-        .attr('y1',center_y)
-        .attr('x2', function(d){
-            return center_x - R*d.A6QUALS1*(Math.cos(Math.floor(Math.random()*360)*(Math.PI/180)))
-        })
-        .attr('y2', function(d){
-            return  center_y - R*d.A6QUALS1*(Math.sin(Math.floor(Math.random()*360)*(Math.PI/180)))
-        })
-        .attr('stroke','red');
-
-    //add the enter() function to make bars for any new countries in the list, and set their properties
-    lines
-        .enter()
-        .append('line')
-        .attr('x1',center_x)
-        .attr('y1',center_y)
-        .attr('x2', function(d){
-            return center_x - R*d.A6QUALS1*(Math.cos(Math.floor(Math.random()*360)*(Math.PI/180)))
-        })
-        .attr('y2', function(d){
-            return center_y - R*d.A6QUALS1*(Math.sin(Math.floor(Math.random()*360)*(Math.PI/180)))
-        })
-        .attr('id', function(d){
-            return 'id' + d.CASEID
-        })
-        .attr('stroke','red')
-        .on('mouseover', function(d){
-            d3.select(this).attr('stroke-width', '10').attr('stroke','purple');
-            var currentId =  d3.select(this).attr('id');
-            svg2.selectAll('#' + currentId).attr('stroke-width', '10').attr('stroke', 'red');
-        })
-        .on('mouseout', function(d){
-            d3.select(this).attr('stroke','red').attr('stroke-width', '1');
-            var currentId =  d3.select(this).attr('id');
-            svg2.selectAll('#' + currentId).attr('stroke', 'purple').attr('stroke-width', '1');
-        });
-
-    var lines2 = svg2.selectAll('line')
-        .data(pointData, function(d){
-            return d.A7GENED;
-        });
-
-    //look to see if there are any old bars that don't have keys in the new data list, and remove them.
-    lines2.exit()
-        .remove();
-
-    //update the properties of the remaining bars (as before)
-    lines2
-        .append('line')
-        .attr('x1',center_x2)
-        .attr('y1',center_y2)
-        .attr('x2', function(d){
-            return center_x2 - R*d.A7GENED*(Math.cos(Math.floor(Math.random()*360)*(Math.PI/180)))
-        })
-        .attr('y2', function(d){
-            return  center_y2 - R*d.A7GENED*(Math.sin(Math.floor(Math.random()*360)*(Math.PI/180)))
-        })
-        .attr('stroke','purple');
-
-    //add the enter() function to make bars for any new countries in the list, and set their properties
-    lines2
-        .enter()
-        .append('line')
-        .attr('x1',center_x2)
-        .attr('y1',center_y2)
-        .attr('x2', function(d){
-            return center_x2 - R*d.A7GENED*(Math.cos(Math.floor(Math.random()*360)*(Math.PI/180)))
-        })
-        .attr('y2', function(d){
-            return center_y2 - R*d.A7GENED*(Math.sin(Math.floor(Math.random()*360)*(Math.PI/180)))
-        })
-        .attr('id', function(d){
-            return 'id' + d.CASEID
-        })
-        .attr('stroke','purple');
 
 
 }
 
-function buttonClicked(){
+/* function buttonClicked(){
 
     if(clicked == true){
         drawPoints(currentDancers);
@@ -279,3 +152,4 @@ function buttonClicked(){
         clicked = true;
     }
 }
+*/
